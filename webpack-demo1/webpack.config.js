ac5fff251154a541e47d667a3bf6db64a246cb47
Henery <Henery_002@163.com>
2017-09-21 13:35:05 +0800

    update

--- webpack-demo1/webpack.config.js
+++ webpack-demo1/webpack.config.js
@@ -9,12 +9,25 @@ module.exports = {
 		a:'./src/script/a.js'
 	},
 	output:{
-		path:'./dist',				//指定打包文件的存放位置
-		filename:'js/[name]-[chunkhash].js'			//指定打包文件的文件名
+		// path:'./dist',				//指定打包文件的存放位置
+		filename:'js/[name]-[chunkhash].js',			//指定打包文件的文件名
+		publicPath:'http://cdn.com/'
 	},
 	plugins:[
 		new htmlWebpackPlugin({
-			template:'index.html'
+			filename:'index-[hash].html',
+			// filename:'index.html',
+			template:'index.html',
+			//传参
+			// inject:'body',
+			inject:false,
+			title:'webpack is good',
+			date:new Date(),
+			//对生成文件压缩
+			minify:{
+				removeComments:true,				//删除注释
+				collapseWhitespace:true				//删除空格
+			}
 		})
 	]
 }
